240bdcffa8d1fc39f73a94790c9c7ee3e55943f5
daalfox <daalfox@gmail.com>
2023-02-12 17:24:11 +0330

    added termguicolors

--- lua/eye_candy.lua
+++ lua/eye_candy.lua
@@ -1,3 +1,4 @@
+vim.opt.termguicolors = true
 require("catppuccin").setup({
     flavour = "macchiato", -- latte(light), frappe(dark low contrast), macchiato(dark medium contrast), mocha(dark high contrast)
     background = {
