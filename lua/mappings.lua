818058b8f8adbd13b71a1dbd5064e20c4e9fce84
daalfox <daalfox@gmail.com>
2023-02-12 16:37:48 +0330

    next/prev hunk mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('n', '<F1>', ':Telescope find_files<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<F2>', ':Telescope buffers<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gn', ':Gitsigns next_hunk<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gp', ':Gitsigns prev_hunk<CR>', { noremap = true })
