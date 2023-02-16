4d3efac95681159ee02b9d2790530182a6e56a0c
daalfox <daalfox@gmail.com>
2023-02-16 10:20:24 +0330

    only forward + html snippets for tsx

--- lua/luasnip_cfg.lua
+++ lua/luasnip_cfg.lua
@@ -1,28 +1,14 @@
-require("luasnip.loaders.from_vscode").lazy_load()
-
 local luasnip = require("luasnip")
-local cmp = require("cmp")
+
+luasnip.filetype_extend("typescriptreact", { "html" })
+require("luasnip/loaders/from_vscode").lazy_load()
 
 luasnip.setup({
-    update_events = 'TextChanged, TextChangedI';
+    update_events = 'TextChanged, TextChangedI',
 })
 
-cmp.setup({
-    mapping = {
-        ["<C-]>"] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-
-        ["<C-[>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    },
-})
+vim.keymap.set({ "i", "s" }, "<C-]>", function()
+    if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+    end
+end, { silent = true })
