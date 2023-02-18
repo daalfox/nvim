4fde6f2df62b30b3fad3ddbc2e5b243cf38ba426
daalfox <daalfox@gmail.com>
2023-02-18 12:17:38 +0330

    format svelte with prettier

--- lua/null-ls_cfg.lua
+++ lua/null-ls_cfg.lua
@@ -2,6 +2,8 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({
+            extra_filetypes = { "svelte" },
+        }),
     },
 })
